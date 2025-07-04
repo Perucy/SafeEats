eb79e5f6c87a7b9e0f1b7e6808dc2b02095b5137
Perucy <perucymussiba@gmail.com>
2025-07-03 23:04:56 -0700

    some image preprocessing

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pytesseract
 import cv2
 import os
-from PIL import Image
+from PIL import Image, ImageEnhance
 
 # to do
 # 1. image pre-processing before text extraction
@@ -14,6 +14,12 @@ def extract_text_from_image(file_path: str)->str:
     # convert to grayscale
     gray = img.convert('L')
 
+    #image resizing
+    img = resize_images(gray)
+
+    # image brightness and contrast adjustment
+    img = image_brightness_contrast(img)
+
     # config
     config = ('-l eng --oem 1 --psm 3')
 
@@ -23,8 +29,39 @@ def extract_text_from_image(file_path: str)->str:
     text = pytesseract.image_to_string(img, config=config)
 
     return text
+
+def resize_images(image, min_height=40, min_width=20):
+    width, height = image.size
+
+    if height < 300:
+        scale_ftr = 300 / height
+    elif height > 2000:
+        scale_ftr = 1200 / height
+    else:
+        scale_ftr = 1.0
+
+    if scale_ftr != 1.0:
+        new_width = int(width * scale_ftr)
+        new_height = int(height * scale_ftr)
+        res_img = image.resize((new_width, new_height), Image.Resampling.LANCZOS)
+        print(f"old size: {width}x{height}, new size: {new_width}x{new_height}")
+    else:
+        res_img = image
+        print(f"Image size is already optimal: {width}x{height}")
+
+    return res_img
+
+def image_brightness_contrast(image, brightness=1.1, contrast=1.1):
+    bright = ImageEnhance.Brightness(image)
+    bright_img = bright.enhance(brightness)
+
+    cont = ImageEnhance.Contrast(bright_img)
+    cont_img = cont.enhance(contrast)
+
+    return cont_img
+
 if __name__ == "__main__":
-    print("SafeEats is running...")
+    print("SafeEats is running...\n\n")
     imgs = ['img1.jpg', 'img2.jpg', 'img3.jpg', 'img4.webp', 'img5.jpg', 'img6.webp', 'img7.jpg']
     
     imgs_dir = os.path.join(os.path.dirname(__file__), "test_images")
